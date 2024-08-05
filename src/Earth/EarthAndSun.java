dca7832395938cc883349b91ea965ff873a6256a
Ursuzev Boris <BorerKicker@yandex.ru>
2024-08-05 08:16:27 +0300

    Finish project Earht and Sun

--- src/Earth/EarthAndSun.java
+++ src/Earth/EarthAndSun.java
@@ -3,30 +3,55 @@ package Earth;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 
 
-public class EarthAndSun extends JPanel {
-    private int xSun, ySun, xEarth, yEarth;
+
+public class EarthAndSun extends JPanel implements ActionListener {
+    private int xSun = 175;
+    private int ySun = 175;
+    private int x0 = 225;
+    private int y0 = 225;
+    private int radius = 150;
+
+
+    private Timer time;
+    private double t = 0;
+    private int xEarth = 150;
+    private int yEarth = 150;
     private BufferedImage sun, earth;
     public EarthAndSun(){
         try{
-            FileInputStream img = new FileInputStream(new File("D:\\java developer\\GUI tasks\\src\\Earth\\earth.png"));
+            FileInputStream img = new FileInputStream(new File("F:\\курс разработчик\\курс java\\GUI-tasks\\src\\Earth\\earth.png"));
             earth = ImageIO.read(img);
-            img = new FileInputStream(new File("D:\\java developer\\GUI tasks\\src\\Earth\\sun.png"));
+            img = new FileInputStream(new File("F:\\курс разработчик\\курс java\\GUI-tasks\\src\\Earth\\sun.png"));
             sun = ImageIO.read(img);
         }catch (Exception ex){
             ex.printStackTrace();
         }
+        time = new Timer(10, this);
     }
     @Override
-    public void paint(Graphics g){
-        super.paint(g);
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         setBackground(Color.BLACK);
-        g2d.drawImage(sun, 200, 200, 200, 200, this);
-        g2d.drawImage(earth, 100,100, 120,100, this);
+
+        g2d.drawImage(sun, xSun, ySun, 150, 150, this);
+        g2d.drawImage(earth, xEarth,yEarth, 60,50, this);
+        time.start();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        double angle = t * 2.0 * Math.PI / 360.;
+        xEarth = x0 - (int)(radius * Math.cos(angle));
+        yEarth = y0 - (int)(radius * Math.sin(angle));
+        t ++;
+        repaint();
     }
 }
