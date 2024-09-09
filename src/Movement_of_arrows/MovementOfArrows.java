2d6e14d45017908db5c87a469e22c372e7dcf75b
Ursuzev Boris <BorerKicker@yandex.ru>
2024-09-09 06:24:57 +0300

    modified the program to teleport the picture

--- src/Movement_of_arrows/MovementOfArrows.java
+++ src/Movement_of_arrows/MovementOfArrows.java
@@ -38,28 +38,40 @@ public class MovementOfArrows extends JPanel implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT ) {
+            if(x > 0) {
+                x -= 50;
+            }else{
+                x = 750;
+            }
 
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if(x < 750) {
+                x += 50;
+            }else{
+                x = 0;
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            if( y > 0) {
+                y -= 50;
+            }else{
+                y = 550;
+            }
+        }
+        if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            if(y < 550) {
+                y += 50;
+            }else{
+                y = 0;
+            }
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_LEFT && x > 0) {
-            x -= 50;
 
-        }
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT && x < 750) {
-            x += 50;
-
-        }
-        if (e.getKeyCode() == KeyEvent.VK_UP && y > 0) {
-            y -= 50;
-
-        }
-        if(e.getKeyCode() == KeyEvent.VK_DOWN && y < 550){
-            y += 50;
-
-
-        }
 
     }
 }
