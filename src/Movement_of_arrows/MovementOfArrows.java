7bc192db59e65f30b5253f77565c708fd34537c2
HappyBorer <BorerKicker@yandex.ru>
2024-09-16 08:29:43 +0300

    added an accelerating movement

--- src/Movement_of_arrows/MovementOfArrows.java
+++ src/Movement_of_arrows/MovementOfArrows.java
@@ -12,11 +12,12 @@ import java.io.IOException;
 public class MovementOfArrows extends JPanel implements KeyListener {
     private int x = 0, y = 0;
     private BufferedImage img;
+    private int speed = 50;
 
     MovementOfArrows() {
 
         try {
-            img = ImageIO.read(new File("F:\\курс разработчик\\курс java\\GUI-tasks\\src\\Movement_of_arrows\\smil.png"));
+            img = ImageIO.read(new File("D:\\java developer\\GUI tasks\\src\\Movement_of_arrows\\smil.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,35 +39,75 @@ public class MovementOfArrows extends JPanel implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_LEFT ) {
-            if(x > 0) {
-                x -= 50;
-            }else{
-                x = 750;
+        if(e.isShiftDown()){
+            if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                if (x > 0 && x != speed) {
+                    x -= speed*2;
+                } else if(x == speed){
+                    x = 750;
+                } else {
+                    x = 750 - speed;
+                }
             }
+            if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                if (x < 750 && x != (750 - speed)) {
+                    x += speed*2;
+                } else if(x == (750 - speed)){
+                    x = 0;
+                }else {
+                    x = speed;
+                }
+            }
+            if (e.getKeyCode() == KeyEvent.VK_UP) {
+                if (y > 0 && y != speed) {
+                    y -= speed*2;
+                } else if(y == speed){
+                    y = 550;
+                }else {
+                    y = 550 - speed;
+                }
+            }
+            if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                if (y < 550 && y != (550 - speed)) {
+                    y += speed*2;
+                } else if(y == (550 - speed)){
+                    y = 0;
+                }else {
+                    y = speed;
+                }
+            }
+        }else{
+            if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                if (x > 0) {
+                    x -= speed;
+                } else {
+                    x = 750;
+                }
 
-        }
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            if(x < 750) {
-                x += 50;
-            }else{
-                x = 0;
+            }
+            if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                if (x < 750) {
+                    x += speed;
+                } else {
+                    x = 0;
+                }
+            }
+            if (e.getKeyCode() == KeyEvent.VK_UP) {
+                if (y > 0) {
+                    y -= speed;
+                } else {
+                    y = 550;
+                }
+            }
+            if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                if (y < 550) {
+                    y += speed;
+                } else {
+                    y = 0;
+                }
             }
         }
-        if (e.getKeyCode() == KeyEvent.VK_UP) {
-            if( y > 0) {
-                y -= 50;
-            }else{
-                y = 550;
-            }
-        }
-        if(e.getKeyCode() == KeyEvent.VK_DOWN){
-            if(y < 550) {
-                y += 50;
-            }else{
-                y = 0;
-            }
-        }
+
     }
 
     @Override
