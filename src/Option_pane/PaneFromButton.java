29c6416f5c28463cf6ac2652ae4e81fe0453157a
Ursuzev Boris <BorerKicker@yandex.ru>
2024-10-02 07:22:45 +0300

    i finished task with two questions

--- src/Option_pane/PaneFromButton.java
+++ src/Option_pane/PaneFromButton.java
@@ -13,9 +13,21 @@ public class PaneFromButton extends JPanel implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            String name = JOptionPane.showInputDialog(this, "My name", "Massage",
-                    JOptionPane.QUESTION_MESSAGE);
-            JOptionPane.showMessageDialog(this, name, "you name", JOptionPane.INFORMATION_MESSAGE);
+            int one = JOptionPane.showConfirmDialog(this, "Dose do you need it?", "Question one",
+                    JOptionPane.YES_NO_OPTION);
+            int two = JOptionPane.showConfirmDialog(this, "Do you have many?", "Question two",
+                    JOptionPane.YES_NO_OPTION);
+            String answer  = "";
+            if(one == 0 && two == 0){
+                answer = "You buy it";
+            } else if (one == 1 && two == 0) {
+                answer = "You keep many";
+            } else if (one == 0 && two == 1){
+                answer = "You go to work";
+            } else {
+                answer = "You think about job";
+            }
+            JOptionPane.showMessageDialog(this, answer, "you name", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
