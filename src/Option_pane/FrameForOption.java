1152895efe00e556a7f504238a4d12be40434901
Ursuzev Boris <BorerKicker@yandex.ru>
2024-10-04 07:45:32 +0300

    i finished a task resolition is choice

--- src/Option_pane/FrameForOption.java
+++ src/Option_pane/FrameForOption.java
@@ -4,17 +4,25 @@ import javax.swing.*;
 import java.awt.*;
 
 public class FrameForOption extends JFrame {
-    FrameForOption(){
+    private int width;
+    private int height;
+
+
+    FrameForOption(int width, int height) {
         super("Option pane");
+        this.width = width;
+        this.height = height;
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setDefaultLookAndFeelDecorated(true);
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-        setBounds(screen.width/2-400, screen.height/2-200, 800, 400);
+        setBounds(screen.width / 2 - width / 2, screen.height / 2 - height / 2, width, height);
         Container container = getContentPane();
         PaneFromButton pane = new PaneFromButton();
         container.add(pane);
         addKeyListener(pane);
+        revalidate();
         setVisible(true);
+
     }
 
 }
--- src/Option_pane/Main.java
+++ src/Option_pane/Main.java
@@ -1,7 +1,46 @@
 package Option_pane;
 
+import javax.swing.*;
+
 public class Main {
+    private static int width, height;
+
     public static void main(String[] args) {
-        new FrameForOption();
+        Object[] resolutions = {"800x600", "1024x768", "1200x600", "1280x1024", "1680x1050", "1920x1080"};
+        String resolution = (String) JOptionPane.showInputDialog(null, "You select the screen resolution",
+                "Screen resolution", JOptionPane.QUESTION_MESSAGE, null, resolutions, "800x600");
+        try {
+            switch (resolution) {
+                case "800x600" -> {
+                    width = 800;
+                    height = 600;
+                }
+                case "1024x768" -> {
+                    width = 1024;
+                    height = 768;
+                }
+                case "1200x600" -> {
+                    width = 1200;
+                    height = 600;
+                }
+                case "1280x1024" -> {
+                    width = 1280;
+                    height = 1024;
+                }
+                case "1680x1050" -> {
+                    width = 1680;
+                    height = 1050;
+                }
+                case "1920x1080" -> {
+                    width = 1920;
+                    height = 1080;
+                }
+            }
+
+            new FrameForOption(width, height);
+        }catch (NullPointerException ex){
+
+        }
+
     }
 }
