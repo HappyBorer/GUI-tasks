226d39cb62408b313217ad3959d74ceec34c6950
Ursuzev Boris <BorerKicker@yandex.ru>
2025-01-02 10:55:08 +0300

    i did a query 'insert'

--- src/SQL2/Main.java
+++ src/SQL2/Main.java
@@ -2,19 +2,61 @@ package SQL2;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
+    private static Statement statement;
+    private static String urlDriverSQLite = "org.sqlite.JDBC";
+    private static String urlDB = "jdbc:sqlite:F:\\курс разработчик\\курс java\\GUI-tasks\\src\\SQL2\\My_cats.db";
+    private static Connection con;
     public static void main(String[] args) {
+        connectionDB();
+        insert_type("INSERT INTO 'types'(type) VALUES('Абиссинская кошка')");
+        insert_type("INSERT INTO 'types'(type) VALUES('Австралийский мист')");
+        insert_type("INSERT INTO 'types'(type) VALUES('Американская жесткошерстная')");
+        closeAll();
+    }
+
+    public static void connectionDB(){
         try {
-            Class.forName("org.sqlite.JDBC");
-            Connection con = DriverManager.getConnection("jdbc:sqlite:F:\\курс разработчик\\курс java" +
-                    "\\GUI-tasks\\src\\SQL2\\My_cats.db");
-            Statement statement = con.createStatement();
-            statement.execute("CREATE TABLE IF NOT exists 'types'(" +
-                    "'id' INTEGER PRIMARY KEY AUTOINCREMENT , 'type' VARCHAR(100) NOT NULL," +
-                    " CONSTRAINT name_unique UNIQUE(id))");
+            Class.forName(urlDriverSQLite);
+            con = DriverManager.getConnection(urlDB);
+            statement = con.createStatement();
+            System.out.println("connection");
         }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void createTable(String query){
+        try {
+            statement.execute(query);
+            System.out.println("created");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void insert_type(String type){
+        try{
+            if(type.startsWith("INSERT")){
+                statement.execute(type);
+                System.out.println("added a row");
+            }else{
+                throw new SQLException("request is not to add");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeAll(){
+        try {
+            con.close();
+            statement.close();
+            System.out.println("closed");
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
