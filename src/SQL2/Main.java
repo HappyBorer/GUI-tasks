3443c9f534eac84a185bda86332815ade84c868a
Ursuzev Boris <BorerKicker@yandex.ru>
2025-01-05 07:07:22 +0300

    I changed a teskadd_all_types

--- src/SQL2/Main.java
+++ src/SQL2/Main.java
@@ -1,15 +1,13 @@
 package SQL2;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     private static Statement statement;
     private static String urlDriverSQLite = "org.sqlite.JDBC";
     private static String urlDB = "jdbc:sqlite:F:\\курс разработчик\\курс java\\GUI-tasks\\src\\SQL2\\My_cats.db";
     private static Connection con;
+    private static ResultSet resSet;
     private static String[] types = new String[]{"Абиссинская кошка",
             "Австралийский мист",
             "Американская жесткошерстная",
@@ -101,11 +99,11 @@ public class Main {
 
     public static void insert_type(String type) {
         try {
-            if (type.startsWith("INSERT")) {
-                statement.execute(type);
+            resSet = statement.executeQuery(String.format("SELECT type FROM types WHERE type = '%s'", type));
+            if (!(resSet.next())) {
+                statement.executeUpdate(String.format("INSERT INTO types(type) VALUES('%s')", type));
                 System.out.println("added a row");
-            } else {
-                throw new SQLException("request is not to add");
+                statement.close();
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -115,7 +113,7 @@ public class Main {
     public static void add_all_types(String[] types) {
         try {
             for (String type : types) {
-                statement.execute(String.format("INSERT INTO 'types'(type) VALUES('%s')", type));
+                insert_type(type);
             }
             System.out.println("Everything added");
         } catch (Exception e) {
@@ -127,6 +125,7 @@ public class Main {
         try {
             con.close();
             statement.close();
+            resSet.close();
             System.out.println("closed");
         } catch (Exception e) {
             e.printStackTrace();
